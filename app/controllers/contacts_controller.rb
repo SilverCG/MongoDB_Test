9f40bc3cdb31a1c0a239f29eb14e98c24ae99bb8
Dave Johnson <dave@silvercg.com>
2015-12-11 13:19:55 -0700

    style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -65,7 +65,7 @@ class ContactsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
-      @contact = Contact.find(params[:id], user: current_user)
+      @contact = Contact.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
